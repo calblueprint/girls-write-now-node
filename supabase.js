0478b1aa709343ae3b6689aae41aa9c29d628a5d
Emily Sunaryo <eadorasunaryo@berkeley.edu>
2023-10-18 11:25:22 -0700

    completed basic porting to supabase framework

--- supabase.js
+++ supabase.js
@@ -1,24 +1,29 @@
 import { createClient } from '@supabase/supabase-js';
-import { filterStories } from './index';
+import "dotenv/config.js";
+import { filterStories, getAllStories } from './index.js';
 
 
-const supabaseUrl = proccess.env.SUPABASE_URL;
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function insertStoryData() {
-    const storyData = filterStories();
+    const unfilteredStoryData = await getAllStories();
+    const storyData = await filterStories(unfilteredStoryData);
+    console.log(storyData);
     storyData.forEach(async obj => {
         const { data, error } = await supabase
         .from('stories')
         .insert([
-        { id: obj.id,
-         date: obj.date, 
-         title: obj.title.rendered, 
-         content: obj.content.rendered, 
-        //  process: 'adfadf',
-         excerpt: obj.excerpt.rendered,
-        featured_media: obj.featured_media  },
+        { 
+            id: obj.id,
+            date: obj.date, 
+            title: obj.title.rendered, 
+            content: obj.content.rendered, //need to push content through html parser 
+             process: obj.content.rendered, //need to extract process content from content after its parsed 
+            excerpt: obj.excerpt.rendered,
+            featured_media: obj.featured_media  //featured media is currently just an ID, need to extract links somehow
+        },
         ])
 
     })
@@ -33,30 +38,3 @@ insertStoryData();
 
 
 
-
-// async function insertData() {
-//     const { data: result, error } = await supabase
-//       .rpc('insert_data', { data });
-  
-//     if (error) {
-//       console.error('Error inserting data:', error);
-//     } else {
-//       console.log('Data inserted successfully:', result);
-//     }
-//   }
-  
-//   insertData();
-
-
-
-
-//supabase backend object looks like:
-//{id:, date:, title:, content:, process:, excerpt:, featured_media:, link:  }
-// const { error } = await supabase
-//   .from('stories')
-//   .insert({ id: 1, title: 'Denmark', content: adfasdfasd, process: adfadsf, excerpt: adfasdf, featured_media:aadsfafda, link:asdfasdfads })
-
-
-
-
-//approach 1: iterate through filteredStories and pull data that I want to see in the database
