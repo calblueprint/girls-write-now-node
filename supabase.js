6ebd2f2da1a360bf00b5aaee5ad28f26c24587af
Akshay Thakur <akshaythakur@berkeley.edu>
2023-10-27 18:41:03 -0700

    code cleanup, error handling

--- supabase.js
+++ supabase.js
@@ -1,137 +1,111 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 import "dotenv/config.js";
-import { decode } from 'html-entities';
-import { createStoryObjects, getFeaturedMedia, htmlParser } from './wordpress.js';
+import { decode } from "html-entities";
+import { getAuthor, getFeaturedMedia, htmlParser } from "./wordpress.js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+const storyTagCateogories = ["tone", "topic", "genre-medium"];
 
-
-async function insertStoryData() {
-    let storyObject;
-    try {
-        storyObject = await createStoryObjects();
-    } finally {
-        storyObject.forEach(async obj => {
-            const htmlParsedObject = htmlParser(obj.content.rendered, obj.excerpt.rendered); 
-            const featuredMediaLink = await getFeaturedMedia(obj.featured_media);
-            await insertStoryTags(obj);
-            await insertCollectionTags(obj);
-            await getAuthor(obj);
-
-            try {
-                const { data, error } = await supabase
-                .from('stories')
-                .upsert([
-                { 
-                    id: obj.id,
-                    date: obj.date, 
-                    title: decode(obj.title.rendered), 
-                    content: htmlParsedObject.story, 
-                     process: htmlParsedObject.process, 
-                    excerpt: htmlParsedObject.excerpt,
-                    featured_media: featuredMediaLink, 
-                    link: obj.link 
-                },
-                ])
-            } catch(error) {
-                console.log(error);
-            } 
-        })
-    }
-}
-
-async function insertStoryTags(storyObject) {
-const categories = ['tone', 'topic', 'genre-medium'];
-    for (const category of categories) {
-      for (const tag of storyObject[category]) {
-        try {
-          const { error } = await supabase
-            .from('stories_tags')
-            .insert([{ story_id: storyObject.id, tag_id: tag }]);
-          console.log(`Added tag ${tag}`);
-        } catch (error) {
-          console.log(`error message: adding story tags into stories_tag schema: ${error}`);
-        }
-      }
+async function insertStories(storyObject) {
+	try {
+		const htmlParsedObject = htmlParser(
+			storyObject.content.rendered,
+			storyObject.excerpt.rendered
+		);
+		const featuredMediaLink = await getFeaturedMedia(
+			storyObject.featured_media
+		);
+		const { error } = await supabase.from("stories").upsert([
+			{
+				id: storyObject.id,
+				date: storyObject.date,
+				title: decode(storyObject.title.rendered),
+				content: htmlParsedObject.story,
+				process: htmlParsedObject.process,
+				excerpt: htmlParsedObject.excerpt,
+				featured_media: featuredMediaLink,
+				link: storyObject.link,
+			},
+		]);
+		if (error) {
+			console.log(
+				`Unable to push story ${storyObject.id} to Supabase: ${error}`
+			);
+		} else {
+			console.log(`Inserted story ${storyObject.id} to Supabase`);
+		}
+	} catch (error) {
+		console.log(`Unable to parse story content: ${error}`);
 	}
 }
 
-async function insertCollectionTags(storyObject) {
-    for (const id of storyObject['collection']) {
-        try {
-         const {error} = await supabase
-          .from('collections_stories')
-          .insert([{story_id: storyObject.id, collection_id: id }])
-           console.log(`Added collection tag ${tag}`);
-        } catch (error) {
-          console.log(`error message: adding story tags into collections_stories schema: ${error}`);
-            }
-        }
+async function insertStoriesTags(storyObject) {
+	for (const category of storyTagCateogories) {
+		for (const tag of storyObject[category]) {
+			const { error } = await supabase
+				.from("stories_tags")
+				.insert([{ story_id: storyObject.id, tag_id: tag }]);
+			if (error) {
+				`Unable to insert tags for story ${storyObject.id}: ${error}`;
+			}
+		}
+	}
+	console.log(`Inserted tags for story ${storyObject.id}`);
 }
 
-const gwnEmail = process.env.GWN_USERNAME;
-const gwnPassword = process.env.GWN_PASSWORD; 
-const headers = new Headers();
-headers.set('Authorization', 'Basic ' + Buffer.from(gwnEmail + ":" + gwnPassword).toString('base64')); //can i move this outside the function?
-
-
-async function getAuthor(storyObject) {
-    const re = /\d+/; //regex to extract integers from string 
-    const endpoint = "https://girlswritenow.org/wp-json/wp/v2/story/" + `${storyObject.id}`;
-    const coauthorObject = await fetch(endpoint); 
-    const coauthorObjectJson = await coauthorObject.json();
-    for (const coauthor of coauthorObjectJson.coauthors) {
-        const coauthorUrl = `https://girlswritenow.org/wp-json/wp/v2/coauthors/${coauthor}`;
-        const coauthorResponse = await fetch(coauthorUrl, {
-            method:'GET',
-            headers: headers, //global headers
-       })
-        const responseJson = await coauthorResponse.json();
-        const authorId = re.exec(responseJson.description)[0]; 
-        const authorMetadataUrl = `https://girlswritenow.org/wp-json/elm/v1/guest-authors/${authorId}`;
-        const metadataResponse = await fetch(authorMetadataUrl, {
-            method:'GET',
-            headers: headers,
-        })
-        const metadataResponseJson = await metadataResponse.json();
-        insertAuthors(metadataResponseJson, coauthor );
-      }
-    
-}
-async function insertAuthors(authorObject, id) {
-    const thumbnailResponse = await fetch (`https://girlswritenow.org/wp-json/wp/v2/media/${authorObject.metadata._thumbnail_id[0]}`)
-    const thumbnailResponseJson = await thumbnailResponse.json();
-    const thumbnailJpeg = thumbnailResponseJson.guid.rendered;
-    try {
-        const {data, error} = await supabase
-         .from('authors')
-         .insert(
-            {
-                id: id, 
-                name: authorObject.post_title,
-                pronouns: authorObject.metadata.pronouns[0],
-                bio: authorObject.metadata["cap-description"][0],
-                artist_statement: authorObject.metadata.artist_statement[0],
-                thumbnail: thumbnailJpeg
-            })
-        console.log(`Added author information ${id}`);
-       } catch (error) {
-         console.log(`error message: could not add author information to supabase ${error}`);
-           }
-
+async function insertCollectionsStories(storyObject) {
+	for (const id of storyObject["collection"]) {
+		const { error } = await supabase
+			.from("collections_stories")
+			.upsert([{ story_id: storyObject.id, collection_id: id }]);
+		if (error) {
+			console.log(
+				`Unable to insert story ${storyObject.id} to collection ${id}: ${error}`
+			);
+		}
+	}
+	console.log(`Inserted story ${storyObject.id} into respective collections`);
 }
 
+async function insertAuthors(storyObject) {
+	try {
+		const authorObject = await getAuthor(storyObject);
+		const { error } = await supabase.from("authors").upsert({
+			id: authorObject.id,
+			name: authorObject.name,
+			pronouns: authorObject.pronouns,
+			bio: authorObject.bio,
+			artist_statement: authorObject.artist_statement,
+			thumbnail: authorObject.thumbnail,
+		});
+		if (error) {
+			console.log(`Unable to insert author ${authorObject.id}: ${error}`);
+		}
+		await insertStoriesAuthors(storyObject.id, authorObject.id);
+		console.log(`Inserted author ${authorObject.id}`);
+	} catch (error) {
+		console.log(`Unable to get author from WP: ${error}`);
+	}
+}
+
+async function insertStoriesAuthors(storyID, authorID) {
+	const { error } = await supabase.from("stories_authors").upsert({
+		story_id: storyID,
+		author_id: authorID,
+	});
+	if (error) {
+		console.log(
+			`Unable to insert author ${authorID} to story ${storyID}: ${error}`
+		);
+	}
+	console.log(`Inserted author ${authorID} to story ${storyID}`);
+}
 
 export {
-    insertStoryData
+	insertAuthors,
+	insertCollectionsStories,
+	insertStories,
+	insertStoriesTags,
 };
-  
-
-
-
-
-
-
-
--- wordpress.js
+++ wordpress.js
@@ -1,132 +1,163 @@
-// Code goes here!
-import { decode } from 'html-entities';
-import fetch from 'node-fetch';
+import { decode } from "html-entities";
+import fetch from "node-fetch";
 
-const storyRoute = 'https://girlswritenow.org/wp-json/wp/v2/story/\?per_page=20';
-const genreMediumRoute = 'https://girlswritenow.org/wp-json/wp/v2/genre-medium/';
+const allStoriesRoute =
+	"https://girlswritenow.org/wp-json/wp/v2/story/?per_page=20";
+const genreMediumRoute =
+	"https://girlswritenow.org/wp-json/wp/v2/genre-medium/";
+const mediaRoute = "https://girlswritenow.org/wp-json/wp/v2/media/";
+const coauthorsRoute = "https://girlswritenow.org/wp-json/wp/v2/coauthors/";
+const authorBioRoute =
+	"https://girlswritenow.org/wp-json/elm/v1/guest-authors/";
 
+const gwnEmail = process.env.GWN_USERNAME;
+const gwnPassword = process.env.GWN_PASSWORD;
+const headers = new Headers();
+headers.set(
+	"Authorization",
+	"Basic " + Buffer.from(gwnEmail + ":" + gwnPassword).toString("base64")
+);
 
-//function fetches story object from wordpress story endpoint
+/* Fetch all story objects from WP story endpoint. */
 const getAllStories = async () => {
-    try {
-      const response = await fetch(storyRoute);
-      const responseJson = await response.json();
-      return responseJson;
-    } catch (error) {
-      console.log('Error');
-      throw error;
-    }
-  };
+	const response = await fetch(allStoriesRoute);
+	const responseJson = await response.json();
+	return responseJson;
+};
 
-  //function fetches genre medium object from wordpress genreMedium endpoint
-  const getAllGenreMediums = async () => {
-    try {
-      const response = await fetch(genreMediumRoute);
-      const responseJson = await response.json();
-    //   console.log(responseJson);
-      return responseJson;
-    } catch (error) {
-      console.log('Error');
-      throw error;
-    }
-  }; 
+/* Fetch all genre-medium IDs from WP genreMedium endpoint. */
+const getAllGenreMediums = async () => {
+	const response = await fetch(genreMediumRoute);
+	const responseJson = await response.json();
+	return responseJson;
+};
 
-
-//this function removes specific story objects after being passed in an array of indexes
+/* Remove elements with indexes in indexes from arr. */
 export function removeElementsByIndexes(arr, indexes) {
-    // Sort the indexes in descending order to prevent issues when removing elements
-    indexes.sort((a, b) => b - a);
-    for (const index of indexes) {
-      if (index >= 0 && index < arr.length) {
-        arr.splice(index, 1);
-      }
-    }
-    return arr;
-  }
-
-//function will filter out non-text story objects
-const filterStories = async (storyObject) => {
-  const genreMediumDict = {};
-    const genreMediumResponse = await getAllGenreMediums();
-    genreMediumResponse.map((obj) => {
-        genreMediumDict[obj.id] = obj.name;
-    });
-    const idArray = [344, 383, 405, 414, 416, 470, 478, 503, 564, 568, 635, 712, 1202, 1205, 1216, 1315, 1350, 1351, 1771, 1804, 1885, 2437, 2438];
-    const indexList = [];
-    for (const id of idArray) {
-      storyObject.forEach((obj, index) => {
-        if( obj["genre-medium"].includes(id)) {
-          indexList.push(index);
-        }
-      })
-    }
-    console.log(indexList);
-    const filteredData = removeElementsByIndexes(storyObject, indexList);
-    return filteredData;
-  }
-
-  //function will return featured media link associated with each story (usually an image/jpeg)
-  const getFeaturedMedia = async (featuredmediaId) => {
-    const featuredMediaLink = 'https://girlswritenow.org/wp-json/wp/v2/media/' + `${featuredmediaId}`;
-    const response = await fetch(featuredMediaLink);
-    const responseJson = await response.json();
-    return responseJson.link;
-
-  }
-
-  //function will apply regex functions to storyObject outputs such as process, story, heading, etc. 
-  function htmlParser(htmlString, htmlExcerpt) {
-    const regexHeading = /(<h2(.*?)h2>)/;
-    const regexStory = /(\n+<p(.*?)p>)+/;
-    const regexProcess = /<p>&nbsp(.*?)p>/;
-    const regexExcerpt = /<p>(.*?)<\/p>/;
-  
-    const heading = regexHeading.exec(htmlString);
-    const story = regexStory.exec(htmlString);
-    const process = regexProcess.exec(htmlString);
-    const excerpt = regexExcerpt.exec(htmlExcerpt);
-  
-    const contentHeading = heading
-      ? decode(heading[0])
-      .replace('</h2>', '')
-      .replace(/<h2.+>/, '')
-      : '';
-
-    const contentStory = story
-      ? decode(story[0])
-      .replace(/(\n)+/gi, '')
-      .replace(/<p>/gi, '')
-      .replace(/<\/p>/gi, '\n\n')
-      : '';
-    const contentProcess = process
-      ? decode(process[0])
-      .replace(/<p>/gi, '')
-      .replace(/<\/p>/gi, '')
-      : '';
-    const contentExcerpt = excerpt
-      ? decode(excerpt[0])
-      .replace(/<p>/gi, '')
-      .replace(/<\/p>/gi, '')
-      : '';
-    return {
-      heading: contentHeading,
-      story: contentStory,
-      process: contentProcess,
-      excerpt: contentExcerpt
-    };
-  }
-
-
-  //this function creates a story Object after running it through filterStories() and removing non-text related stories
-  async function createStoryObjects() {
-    const unfilteredStoryDataObject = await getAllStories();
-    const storyData = await filterStories(unfilteredStoryDataObject);
-    return storyData;
+	// Sort the indexes in descending order to prevent issues when removing elements
+	indexes.sort((a, b) => b - a);
+	for (const index of indexes) {
+		if (index >= 0 && index < arr.length) {
+			arr.splice(index, 1);
+		}
+	}
+	return arr;
 }
 
-export { createStoryObjects, getFeaturedMedia, htmlParser };
+/* Filter story objects to be only text-based stories. */
+const filterStories = async (storyObjects) => {
+	// Use to manually update nonTextGenres if needed
+	// const genreMediumDict = {};
+	// const genreMediumResponse = await getAllGenreMediums();
+	// genreMediumResponse.map((obj) => {
+	//   genreMediumDict[obj.id] = obj.name;
+	// });
 
+	// Hardcoded list of non-text based genre-medium IDs
+	const nonTextGenres = [
+		344, 383, 405, 414, 416, 470, 478, 503, 564, 568, 635, 712, 1202, 1205,
+		1216, 1315, 1350, 1351, 1771, 1804, 1885, 2437, 2438,
+	];
+	const indexesToRemove = [];
+	storyObjects.forEach((storyObject, index) => {
+		storyObject["genre-medium"].forEach((genreID) => {
+			if (nonTextGenres.includes(genreID)) {
+				indexesToRemove.push(index);
+			}
+		});
+	});
+	const filteredData = removeElementsByIndexes(storyObjects, indexesToRemove);
+	return filteredData;
+};
 
+/* Fetch featured media link for each story. */
+const getFeaturedMedia = async (featuredmediaId) => {
+	const featuredMediaLink = mediaRoute + `${featuredmediaId}`;
+	const response = await fetch(featuredMediaLink);
+	const responseJson = await response.json();
+	return responseJson.link;
+};
 
+/* Parse storyObject content into Heading, Story, Process, and Excerpt. */
+function htmlParser(htmlString, htmlExcerpt) {
+	const regexHeading = /(<h2(.*?)h2>)/;
+	const regexStory = /(\n+<p(.*?)p>)+/;
+	const regexProcess = /<p>&nbsp(.*?)p>/;
+	const regexExcerpt = /<p>(.*?)<\/p>/;
 
+	const heading = regexHeading.exec(htmlString);
+	const story = regexStory.exec(htmlString);
+	const process = regexProcess.exec(htmlString);
+	const excerpt = regexExcerpt.exec(htmlExcerpt);
 
+	const contentHeading = heading
+		? decode(heading[0])
+				.replace("</h2>", "")
+				.replace(/<h2.+>/, "")
+		: "";
+
+	const contentStory = story
+		? decode(story[0])
+				.replace(/(\n)+/gi, "")
+				.replace(/<p>/gi, "")
+				.replace(/<\/p>/gi, "\n\n")
+		: "";
+	const contentProcess = process
+		? decode(process[0]).replace(/<p>/gi, "").replace(/<\/p>/gi, "")
+		: "";
+	const contentExcerpt = excerpt
+		? decode(excerpt[0]).replace(/<p>/gi, "").replace(/<\/p>/gi, "")
+		: "";
+	return {
+		heading: contentHeading,
+		story: contentStory,
+		process: contentProcess,
+		excerpt: contentExcerpt,
+	};
+}
+
+/* Create storyObject from raw WP story response. */
+async function createStoryObjects() {
+	const unfilteredStoryObjects = await getAllStories();
+	const filteredStoryObjects = await filterStories(unfilteredStoryObjects);
+	return filteredStoryObjects;
+}
+
+/* Fetch authorObject and authorID from WP author endpoints. */
+async function getAuthor(storyObject) {
+	const re = /\d+/; // Regex to extract integers from string
+	for (const coauthor of storyObject.coauthors) {
+		const coauthorsEndpoint = coauthorsRoute + `${coauthor}`;
+		const coauthorResponse = await fetch(coauthorsEndpoint, {
+			method: "GET",
+			headers: headers,
+		});
+		const responseJson = await coauthorResponse.json();
+		const authorId = re.exec(responseJson.description)[0];
+		const authorBioEndpoint = authorBioRoute + `${authorId}`;
+		const authorBioResponse = await fetch(authorBioEndpoint, {
+			method: "GET",
+			headers: headers,
+		});
+		const authorBioResponseJson = await authorBioResponse.json();
+		const thumbnailResponse = await fetch(
+			mediaRoute + `${authorBioResponseJson.metadata._thumbnail_id[0]}`
+		);
+		const thumbnailResponseJson = await thumbnailResponse.json();
+		const thumbnailJpeg = thumbnailResponseJson.guid.rendered;
+		return {
+			id: coauthor,
+			name: authorBioResponseJson.post_title,
+			pronouns: authorBioResponseJson.metadata.pronouns
+				? authorBioResponseJson.metadata.pronouns.length > 0
+					? authorBioResponseJson.metadata.pronouns[0]
+					: ""
+				: "",
+			bio: authorBioResponseJson.metadata["cap-description"][0],
+			artist_statement: authorBioResponseJson.metadata.artist_statement[0],
+			thumbnail: thumbnailJpeg,
+		};
+	}
+}
+
+export { createStoryObjects, getAuthor, getFeaturedMedia, htmlParser };
